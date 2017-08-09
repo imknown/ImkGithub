af0f9c30b965cf71fca9b8b8eced7943b7b3535b
imknown <jinhe@china-tcloud.com>
2017-08-09 16:29:22 +0800

    Modify canary configs.

---
+++ ZMain/ImkGithubApp/src/main/java/net/imknown/imkgithub/MyApplication.kt
@@ -0,0 +1,36 @@
+package net.imknown.imkgithub
+
+import android.app.Application
+import android.content.Context
+import com.github.moduth.blockcanary.BlockCanary
+import com.letv.sarrsdesktop.blockcanaryex.jrt.BlockCanaryEx
+import com.letv.sarrsdesktop.blockcanaryex.jrt.Config
+import com.squareup.leakcanary.LeakCanary
+import net.imknown.imkcode.utils.ProcessUtils
+
+/**
+ * @author imknown on 8/9/17.
+ */
+class MyApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return
+        }
+
+        if (ProcessUtils.isDefaultProcess(this)) {
+            sApplicationContext = applicationContext
+
+            LeakCanary.install(this)
+            BlockCanary.install(this, AppBlockCanaryContext()).start()
+            BlockCanaryEx.install(Config(this))
+        }
+    }
+
+    companion object {
+        lateinit var sApplicationContext: Context
+    }
+}
