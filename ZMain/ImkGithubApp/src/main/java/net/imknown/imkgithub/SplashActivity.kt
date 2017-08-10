115afe2ce925a4735ad434b543a6493be8d706a8
imknown <jinhe@china-tcloud.com>
2017-08-10 13:26:56 +0800

    Add SplashActivity.

---
+++ ZMain/ImkGithubApp/src/main/java/net/imknown/imkgithub/SplashActivity.kt
@@ -0,0 +1,29 @@
+package net.imknown.imkgithub
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+
+import net.imknown.imkgithub.mvp.BaseMvpPresenterActivity
+
+class SplashActivity : BaseMvpPresenterActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        if (isBroughtToFront) {
+            finish()
+            return
+        }
+
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            finish()
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+        }, 2000)
+    }
+
+    private val isBroughtToFront: Boolean
+        get() = intent.flags and Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT != 0
+}
