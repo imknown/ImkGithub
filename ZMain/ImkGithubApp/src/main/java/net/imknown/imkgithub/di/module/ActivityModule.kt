1dca92abe216fd876c8699c7293a118cba0bc1f5
Jin He <jinhe@mapbar.com>
2018-10-24 16:22:00 +0800

    Change param type of ActivityModule to 'BaseActivity'

--- ZMain/ImkGithubApp/src/main/java/net/imknown/imkgithub/di/module/ActivityModule.kt
+++ ZMain/ImkGithubApp/src/main/java/net/imknown/imkgithub/di/module/ActivityModule.kt
@@ -1,14 +1,14 @@
 package net.imknown.imkgithub.di.module
 
-import android.app.Activity
 import dagger.Module
 import dagger.Provides
 import net.imknown.imkgithub.buz.main.MainActivity
 import net.imknown.imkgithub.buz.main.MainMvpContract
 import net.imknown.imkgithub.buz.main.MainMvpPresenter
+import net.imknown.imkgithub.global.BaseActivity
 
 @Module
-class ActivityModule(private val mActivity: Activity) {
+class ActivityModule(private val mActivity: BaseActivity) {
     // region [Main]
     @Provides
     internal fun provideMainMvpContractIView(): MainMvpContract.IView = mActivity as MainActivity
